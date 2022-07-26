0364f7a3339e6fcfbcdc8a25a51c6079241c1506
stahh <lupuslone@hotmail.com>
2022-07-26 12:33:39 +0300

    added tests

---
+++ tests/tests.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+import unittest
+from mock import patch, MagicMock
+import json
+
+from lambda_example.index import lambda_handler, parse
+import lambda_example
+
+DT = datetime.strptime('12:33, 10 May 2022', '%H:%M, %d %B %Y')
+LU = '00:48, 23 July 2022'
+MC = '15'
+S1 = 'wiki'
+S2 = 'xtools'
+
+
+class TestHandler(unittest.TestCase):
+    """
+    Test lambda handler
+    """
+
+    @staticmethod
+    def read(fname):
+        with open(fname, 'r') as inp:
+            return inp.read()
+
+    @patch('lambda_example.index._get')
+    def test_request(self, mocked_get):
+        mocked_get.return_value = LU
+        wiki = lambda_example.index._get(
+            'https://en.wikipedia.org/w/index.php', S1,
+            {'title': 'Washington,_D.C.', 'action': 'history'})
+        self.assertEqual(LU, wiki)
+        mocked_get.return_value = MC
+        xtools = lambda_example.index._get(
+            'https://xtools.wmflabs.org/articleinfo/en.wikipedia.org/'
+            'Washington,_D.C./2022-07-01/2022-07-23#month-counts', S2)
+        self.assertEqual(MC, xtools)
+
+    @patch('lambda_example.index.get_latest_update',
+           MagicMock(return_value=DT))
+    @patch('lambda_example.index.get_month_count', MagicMock(return_value=13))
+    @patch('lambda_example.index.save', MagicMock(return_value=None))
+    def test_response(self):
+        """Test handler main function for successfully response"""
+        response = lambda_handler(
+            {'queryStringParameters': {'title': 'Washington,_D.C.'}}, None)
+        self.assertEqual(200, response['statusCode'])
+        self.assertEqual(
+            json.loads(response['body'])['message'],
+            f'File Washington,_D.C..json saved successful. '
+            f'Latest update: {DT}. '
+            f'Month counts: 13')
+
+    def test_parsing(self):
+        wiki = parse(self.read(f'{S1}.html'), S1)
+        self.assertEqual(LU, wiki)
+        xtools = parse(self.read(f'{S2}.html'), S2)
+        self.assertEqual(MC, xtools)
+
+
+if __name__ == "__main__":
+    unittest.main()
